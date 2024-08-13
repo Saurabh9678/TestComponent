f2340346560ed3c163e0bba48b275a2ba6974c97
Saurabh9678 <saurabh.gohain@daakia.co.in>
2024-08-13 17:39:25 +0530

    Use state change

--- src/TestComponent.js
+++ src/TestComponent.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const TestComponent = () => {
-  return <div>Hello from TestComponent!</div>;
+const TestComponent = ({name}) => {
+  const [username, setUsername] = React.useState("");
+  React.useEffect(() => {
+    if(name){
+      setUsername(name);
+    }else{
+      setUsername("Guest");
+    }
+  },[name]);
+  return (
+    <div>
+      <div>Hello from TestComponent!</div>
+      <h1>{username}</h1>
+    </div>
+  );
+
 };
 
 export default TestComponent;
